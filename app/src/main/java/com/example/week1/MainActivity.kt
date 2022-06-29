eb064f9d50e0981b8f6a53b569bc13965fdcb7b7
alex6095 <l.alex6095@gmail.com>
2022-06-29 23:26:47 +0900

    Tab with Pager2

--- app/src/main/java/com/example/week1/MainActivity.kt
+++ app/src/main/java/com/example/week1/MainActivity.kt
@@ -2,10 +2,38 @@ package com.example.week1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentActivity
+import com.example.week1.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val tabIcon = listOf(
+        R.drawable.noun_contact,
+        R.drawable.noun_gallery
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.viewpager.apply {
+            adapter = MyPagerAdapter(context as FragmentActivity)
+        }
+
+        TabLayoutMediator(binding.tabs, binding.viewpager) { tab, position ->
+            when (position) {
+                0 -> {
+                    tab.text = "Contact"
+                    tab.setIcon(tabIcon[position])
+                }
+                1 -> {
+                    tab.text = "Gallery"
+                    tab.setIcon(tabIcon[position])
+                }
+                else -> tab.text = "TBD"
+            }
+        }.attach()
     }
 }
