5e98efb5c32bd3bee890e2c98c7ead53f09a0749
sangyeob <rlatkdduq99@gmail.com>
2022-06-29 22:00:36 +0900

    listview 연습
    listview 연습
    
    mainactivity와 xml 에 추가

--- app/src/main/java/com/example/week1/MainActivity.kt
+++ app/src/main/java/com/example/week1/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.week1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
 
 class MainActivity : AppCompatActivity() {
+
+    val LIST_MENU = arrayOf("LIST1", "LIST2", "LIST3")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, LIST_MENU)
+
+        val listview = findViewById(R.id.listview1) as ListView
+        listview.setAdapter(adapter)
     }
+
 }
