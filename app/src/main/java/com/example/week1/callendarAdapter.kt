9bc9f647134e83fde4457ee45bb1f371d09ad553
sangyeob <rlatkdduq99@gmail.com>
2022-07-04 23:35:07 +0900

    commit
    commit

--- app/src/main/java/com/example/week1/callendarAdapter.kt
+++ app/src/main/java/com/example/week1/callendarAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.week1
 
+import android.text.Layout
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -7,29 +8,33 @@ import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
+import com.example.week1.databinding.CallenderItem2Binding
 import com.example.week1.databinding.CallenderItemBinding
 import com.l4digital.fastscroll.FastScroller
 
 
 class callendarAdapter(
     val context: CallendarFragment,
-    private val items: ArrayList<Schedule>
-) : RecyclerView.Adapter<callendarAdapter.ViewHolder>() {
+    private val items: ArrayList<Schedule>,
+    val viewtype : Int
+) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
     override fun getItemCount(): Int = items.size
 
-    override fun onBindViewHolder(holder: callendarAdapter.ViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
 
         val item = items[position]
-        val listener = View.OnClickListener { it ->
-            Toast.makeText(it.context, "Clicked -> Name : ${item.content}, Number : ${item.regdata}", Toast.LENGTH_SHORT).show()
+
+        if(viewtype == 0)
+            (holder as ViewHolder).content.text = item.content
+        else
+            (holder as ViewHolder2).content.text = item.content
+
+        holder.itemView.setOnClickListener{
+            if(viewtype == 1)
+                itemClickListener.onClick(it, position)
+
         }
-        holder.apply {
-            bind(listener, item)
-        }
-//        holder.itemView.setOnClickListener{
-//            itemClickListener.onClick(it, position)
-//        }
     }
 
     interface OnItemClickListener{
@@ -42,16 +47,28 @@ class callendarAdapter(
 
     private lateinit var itemClickListener : OnItemClickListener
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val binding = CallenderItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return ViewHolder(binding)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val view : View?
+        return when(viewtype){
+            0 -> {
+                val binding = CallenderItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+                return ViewHolder(binding)
+            }
+            1 -> {
+                val binding = CallenderItem2Binding.inflate(LayoutInflater.from(parent.context), parent, false)
+                return ViewHolder2(binding)
+            }
+            else -> throw RuntimeException("")
+        }
+
     }
 
     // 각 항목에 필요한 기능을 구현
     class ViewHolder(var binding : CallenderItemBinding) : RecyclerView.ViewHolder(binding.root) {
-        fun bind(listener: View.OnClickListener, item: Schedule) {
-            binding.content.text = item.content
-        }
+        val content = binding.content
+    }
+    class ViewHolder2(var binding : CallenderItem2Binding) : RecyclerView.ViewHolder(binding.root) {
+        val content = binding.content
     }
 }
 
